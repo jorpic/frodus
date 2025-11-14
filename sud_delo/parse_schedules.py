1105093b6d8d252ea74b65a6cb63074525ed10fa
Max Taldykin <jorpic@gmail.com>
2025-11-15 00:04:44 +0400

    drop body from errors

--- sud_delo/parse_schedules.py
+++ sud_delo/parse_schedules.py
@@ -7,7 +7,24 @@ import formats.F1 as F1
 import formats.F2 as F2
 from formats.commons import Ok, Err
 
-def parse(body):
+ERR_MSG_1 = 'Информация временно недоступна. Приносим свои извинения. Попробуйте обратиться позже или обратитесь непосредственно в суд.'
+
+ERR_MSG_2 = 'Не определен ни один сервер, на котором расположен модуль сопряжения с БД'
+
+def parse(obj):
+    if 'err' in obj:
+        return Err(obj['err'])
+
+    if obj['status'] != 200:
+        return Err('bad status')
+
+    body = obj['body']
+    if body.find(ERR_MSG_1) != -1:
+        return Err('try again')
+
+    if body.find(ERR_MSG_2) != -1:
+        return Err('DB err')
+
     errs = []
     for p in [F1.parse_cases, F2.parse_cases]:
         res = p(body)
@@ -19,36 +36,10 @@ def parse(body):
             errs.append(('unexpected parser result', res))
     return Err(errs)
 
-
-ERR_MSG_1 = 'Информация временно недоступна. Приносим свои извинения. Попробуйте обратиться позже или обратитесь непосредственно в суд.'
-
-ERR_MSG_2 = 'Не определен ни один сервер, на котором расположен модуль сопряжения с БД'
-
-def get_err(obj):
-    if 'err' in obj:
-        return obj['err']
-
-    if obj['status'] != 200:
-        return 'bad status'
-
-    body = obj['body']
-    if body.find(ERR_MSG_1) != -1:
-        return 'try again'
-
-    if body.find(ERR_MSG_2) != -1:
-        return 'DB err'
-
-
 def main():
     for ln in sys.stdin:
         obj = json.loads(ln)
-        err = get_err(obj)
-        if err:
-            obj['err'] = err
-            print(json.dumps(obj, ensure_ascii=False), file=sys.stderr)
-            continue
-
-        res = parse(obj['body'])
+        res = parse(obj)
         if isinstance(res, Ok):
             sch = {
                 'sud': obj['sud'],
@@ -58,6 +49,7 @@ def main():
             }
             print(json.dumps(sch, ensure_ascii=False))
         else:
+            del obj['body']
             obj['err'] = res.value
             print(json.dumps(obj, ensure_ascii=False), file=sys.stderr)
 
